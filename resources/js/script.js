0ddd6117157e7a693ba3aeecf6fec4a336d4a2b5
Hiro <80069291+HirokiKudo@users.noreply.github.com>
2024-05-30 10:46:47 +1200

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,18 @@
+// if(typeof window === 'object') {
+//     let originText = document.getElementsByClassName('origin');
+//     // let originImage = document.getElementsByClassName('original');
+//     let layer = document.getElementsByClassName('layer');
+// };
+
+// let viewLayer = function(event) {
+//     event.target.style.display = 'block';
+//     event.target.style.display = 'block';
+// };
+
+// originText.addEventListener('click', viewLayer);
+
+// originText.addEventListener('click', function() {
+//     // originImage.style.display = 'block';
+//     document.getElementsByClassName('original').style.display = 'block';
+//     layer.style.display = 'block';
+// });
